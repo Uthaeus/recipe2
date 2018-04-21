af8f9f3f635af57ac33380cb9164a6d94ba8a22f
Uthaeus <romanlavery@gmail.com>
2018-04-21 11:35:54 -0600

    Implemented resources for ingredients and created placeholder image generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415020605) do
+ActiveRecord::Schema.define(version: 20180421165534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.float "quantity"
+    t.string "quant_type"
+    t.bigint "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 20180415020605) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "ingredients", "recipes"
 end
