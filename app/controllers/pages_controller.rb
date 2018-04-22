8b56176452eed6eaed9573def237216a6cf03bde
Uthaeus <romanlavery@gmail.com>
2018-04-21 21:38:06 -0600

    Added menu page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,8 @@ class PagesController < ApplicationController
   def misc
 
   end
+
+  def menu
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   get 'pages/misc'
 
+  get 'pages/menu'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
